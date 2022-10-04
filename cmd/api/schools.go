f74f0c0b74b604a529208db78f7a43f7a3fe2e1e
joanne-y <2019120152@ub.edu.bz>
2022-10-04 06:50:57 -0700

    added the feature to add a school via a JSON request

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -12,14 +13,33 @@ import (
 
 // createSchoolHandler for the "POST /v1/schools" endpoint
 func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new school..")
+	// Our target decvode destination
+	var input struct {
+		Name    string   `json:"name"`
+		Level   string   `json:"level"`
+		Contact string   `json:"contact"`
+		Phone   string   `json:"phone"`
+		Email   string   `json:"email"`
+		Website string   `json:"website"`
+		Address string   `json:"address"`
+		Node    []string `json:"node"`
+	}
+	// Initialize a new json.Decoder instance
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	// Display the request
+	fmt.Fprintf(w, "%+v\n", input)
+
 }
 
 // showSchoolHnadler for the "GET v1/schools/:id" endpoint
 func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 	// Create a new instance of the School struct containing the ID we extracted
@@ -35,9 +55,8 @@ func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request
 		Mode:      []string{"blended", "online"},
 		Version:   1,
 	}
-	err = app.writeJSON(w, http.StatusOK, school, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"school": school}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
