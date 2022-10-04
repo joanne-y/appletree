46ac5a1850473fd421ec3371a37a7f456d183a41
joanne-y <2019120152@ub.edu.bz>
2022-10-04 11:28:33 -0700

    refactored the validation code from the schools.go handler in the cmd/api directory to the schools.go in the internal/data directory

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -24,42 +24,31 @@ func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Reque
 		Address string   `json:"address"`
 		Mode    []string `json:"mode"`
 	}
+
 	// Initialize a new json.Decoder instance
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
+
+	// Copy the values from the input struct to a new School struct
+	school := &data.School{
+		Name:    input.Name,
+		Level:   input.Level,
+		Contact: input.Contact,
+		Phone:   input.Phone,
+		Email:   input.Email,
+		Website: input.Website,
+		Address: input.Address,
+		Mode:    input.Mode,
+	}
+
 	// Initialize a new Validator instance
 	v := validator.New()
-	// Use the Check() method to execute our validation checks
-	v.Check(input.Name != "", "name", "must be provided")
-	v.Check(len(input.Name) <= 200, "name", "must not be more than 200 bytes long")
 
-	v.Check(input.Level != "", "level", "must be provided")
-	v.Check(len(input.Level) <= 200, "level", "must not be more than 200 bytes long")
-
-	v.Check(input.Contact != "", "contact", "must be provided")
-	v.Check(len(input.Contact) <= 200, "contact", "must not be more than 200 bytes long")
-
-	v.Check(input.Phone != "", "phone", "must be provided")
-	v.Check(validator.Matches(input.Phone, validator.PhoneRX), "phone", "must be a valid phone number")
-
-	v.Check(input.Email != "", "email", "must be provided")
-	v.Check(validator.Matches(input.Email, validator.EmailRX), "email", "must be a valid email address")
-
-	v.Check(input.Website != "", "website", "must be provided")
-	v.Check(validator.ValidWebsite(input.Website), "website", "must be a valid URL")
-
-	v.Check(input.Address != "", "address", "must be provided")
-	v.Check(len(input.Address) <= 500, "address", "must not be more than 500 bytes long")
-
-	v.Check(input.Mode != nil, "mode", "must be provided")
-	v.Check(len(input.Mode) >= 1, "mode", "must contain at least 1 entry")
-	v.Check(len(input.Mode) <= 5, "mode", "must not contain at most 5 entries")
-	v.Check(validator.Unique(input.Mode), "mode", "must not contain duplicate entries")
 	//Check the map to determine if there were any validation errors
-	if !v.Valid() {
+	if data.ValidateSchool(v, school); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
